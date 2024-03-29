02279cf8bc4e9312e20ef16abc84fe4803b41ad1
Ahmed Shan <ahmedshan16@gmail.com>
2024-03-30 03:29:37 +0500

    Added header component

--- layout/header.php
+++ layout/header.php
@@ -1,10 +1,13 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $appConfig->app_name ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= $appConfig->base_url; ?>assets/css/style.min.css?v=<?= uniqid() ?>">
-  </head>
-  <body>
+</head>
+
+<body>
+    <?php include dirname(__DIR__) . '/components/_navbar.inc.php'; ?>
