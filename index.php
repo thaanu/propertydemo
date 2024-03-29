39cdfbb0e8a62a515417b917fc962b8406cdcc0c
Ahmed Shan <ahmedshan16@gmail.com>
2024-03-30 03:28:34 +0500

    Refined routing

--- index.php
+++ index.php
@@ -4,8 +4,22 @@ $requestUri = $_SERVER['REQUEST_URI'];
 
 // Check if the file exit
 $page = "home";
+$requestParams = [];
+$requestUri = ltrim($requestUri, "/");
+
+
 if ( !empty($requestUri) ) {
-	$page = ltrim($requestUri, "/");
+
+	$pages = explode("/", $requestUri);
+	$page = $pages[0];
+	unset($pages[0]);
+	$requestParams = $pages;
+
+	// $routes
+	if ( array_key_exists($page, $routes) ) {
+		$page = $routes[$page];
+	}
+
 }
 $controllerFile = __DIR__ . "/pages/{$page}Page.php";
 if ( ! file_exists($controllerFile) ) {
