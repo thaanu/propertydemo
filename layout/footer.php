a22398b887216b7f7cc7f986aff789384b9f96d2
Ahmed Shan <ahmedshan16@gmail.com>
2024-03-30 04:17:21 +0500

    Refactored code.
    Added footer text and separate app.js for javascript

--- layout/footer.php
+++ layout/footer.php
@@ -1,3 +1,15 @@
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+		<footer>
+			<div class="container">
+				
+				<div class="text-center py-5">
+					<?= $appConfig->app_name ?>
+				</div>
+			</div>
+		</footer>
+
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+        <script src="<?= $appConfig->base_url ?>/assets/js/app.js?v=<?= uniqid(); ?>" ></script>
+	</body>
+
 </html>
