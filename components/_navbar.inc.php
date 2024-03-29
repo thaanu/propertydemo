ae4b2c2198324a1743129170e769241999845127
Ahmed Shan <ahmedshan16@gmail.com>
2024-03-30 03:32:19 +0500

    Rearranged property position and renamed to properties

--- components/_navbar.inc.php
+++ components/_navbar.inc.php
@@ -9,21 +9,21 @@
                 <li class="nav-item">
                     <a class="nav-link <?= isActiveTab('home') ?>" aria-current="page" href="/home">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= isActiveTab('about') ?>" href="/about">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= isActiveTab('contact') ?>" href="/contact">Contact Us</a>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle <?= isActiveTab('properties') ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Property Types
+                        Properties
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="/properties/apartments">Apartments</a></li>
                         <li><a class="dropdown-item" href="/properties/houses">Houses</a></li>
                         <li><a class="dropdown-item" href="/properties/villas">Villas</a></li>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= isActiveTab('about') ?>" href="/about">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= isActiveTab('contact') ?>" href="/contact">Contact Us</a>
                 </li>
             </ul>
         </div>
