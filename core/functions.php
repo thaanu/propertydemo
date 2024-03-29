ab055cef0226295eb016ca1b12c73e76ccd81c1c
Ahmed Shan <ahmedshan16@gmail.com>
2024-03-30 03:29:24 +0500

    Added isActiveTab function and refactored code

--- core/functions.php
+++ core/functions.php
@@ -1,9 +1,16 @@
 <?php 
-function getHeader() {
+function getHeader() : void {
     global $appConfig;
     include dirname(__DIR__) . '/layout/header.php';
 }
-function getFooter() {
+function getFooter() : void {
     global $appConfig;
     include dirname(__DIR__) . '/layout/footer.php';
+}
+function isActiveTab( string $tab ) : string {
+    global $page;
+    if ( $page == $tab ) {
+        return ' active ';
+    }
+    return '';
 }
