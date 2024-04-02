825ec051f9ee928df95b186acb269e22358eeb5a
Ahmed Shan <ahmedshan16@gmail.com>
2024-04-02 06:18:26 +0500

    Initial commit

---
+++ resources/data/properties.php
@@ -0,0 +1,21 @@
+<?php 
+return [
+    [
+        "name" => "Home",
+        "city" => 'Male',
+        'type' => 'Apartment',
+        'price' => '50,000'
+    ],
+    [
+        "name" => "Home 2",
+        "city" => 'Hulhumale',
+        'type' => 'House',
+        'price' => '250,000'
+    ],
+    [
+        "name" => "Lemuni, Daunan Worldwide",
+        "city" => 'Male',
+        'type' => 'Apartment',
+        'price' => '60,000'
+    ]
+];
